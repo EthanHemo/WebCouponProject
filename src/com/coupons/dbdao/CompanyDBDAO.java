60e5b1467d4b1d5a4f4b6368590f8c03d0a2ada7
EthanHemo <ethan.hemo@gmail.com>
2016-08-14 18:21:01 +0300

    fix company return object when created get coupon in same function and tried to angular the file update

--- src/com/coupons/dbdao/CompanyDBDAO.java
+++ src/com/coupons/dbdao/CompanyDBDAO.java
@@ -10,6 +10,7 @@ import java.util.Collection;
 import com.coupons.dao.CompanyDAO;
 import com.coupons.data.Company;
 import com.coupons.data.Coupon;
+import com.coupons.data.CouponType;
 import com.coupons.exceptions.ManagerSQLException;
 import com.coupons.exceptions.ManagerThreadException;
 
@@ -33,7 +34,7 @@ public class CompanyDBDAO implements CompanyDAO {
 	 * @throws ManagerThreadException
 	 */
 	@Override
-	public void createCompany(Company company) throws ManagerSQLException, ManagerThreadException {
+	public Company createCompany(Company company) throws ManagerSQLException, ManagerThreadException {
 
 		String insertSQl = "insert into company(comp_name, password, email) " + "values ('" + company.getCompanyName()
 				+ "', '" + company.getPassword() + "', '" + company.getEmail() + "')";
@@ -41,8 +42,16 @@ public class CompanyDBDAO implements CompanyDAO {
 		try {
 			conn = pool.getConnection();
 			Statement statement = conn.createStatement();
-			if (statement.executeUpdate(insertSQl) == 0) {
+			if (statement.executeUpdate(insertSQl, Statement.RETURN_GENERATED_KEYS) == 0) {
 				throw new ManagerSQLException("company was failed to be insert to the database");
+			}
+			else{
+				ResultSet rs = statement.getGeneratedKeys();
+				if(rs != null && rs.next()){
+					company.setId(rs.getLong(1));
+				}else{
+					throw new ManagerSQLException("company was failed to return a value");
+				}
 			}
 
 		} catch (SQLException e) {
@@ -50,6 +59,8 @@ public class CompanyDBDAO implements CompanyDAO {
 		} finally {
 			pool.returnConnection(conn);
 		}
+		
+		return company;
 
 	}
 
@@ -71,11 +82,12 @@ public class CompanyDBDAO implements CompanyDAO {
 		String deleteCustomerCouponSQL = "delete from customer_coupon where coupon_id in (";
 		String deleteCouponSQL = "delete from coupon where id in (";
 		StringBuilder couponIds = new StringBuilder("");
-		Connection conn = null;
+		Connection connection = null;
 
 		try {
-			conn = pool.getConnection();
-			Statement statement = conn.createStatement();
+			connection = pool.getConnection();
+			connection.setAutoCommit(false);
+			Statement statement = connection.createStatement();
 
 			// Finding all coupons and Delete them
 			String sql = "select * from company_coupon where comp_id =" + company.getId();
@@ -108,13 +120,24 @@ public class CompanyDBDAO implements CompanyDAO {
 
 			// Delete the company.
 			if (statement.executeUpdate(deleteCompanySQL) == 0) {
+				
 				throw new ManagerSQLException("Could not fetch insert record");
 			}
 
 		} catch (SQLException e) {
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				throw new ManagerSQLException(e1.getMessage());
+			}
 			throw new ManagerSQLException(e.getMessage());
 		} finally {
-			pool.returnConnection(conn);
+			try {
+				connection.setAutoCommit(true);
+			} catch (SQLException e) {
+				throw new ManagerSQLException(e.getMessage());
+			}
+			pool.returnConnection(connection);
 		}
 
 	}
@@ -129,12 +152,12 @@ public class CompanyDBDAO implements CompanyDAO {
 	 */
 	@Override
 	public void updateCompany(Company company) throws ManagerSQLException, ManagerThreadException {
-		Connection conn = null;
+		Connection connection = null;
 		try {
-			conn = pool.getConnection();
+			connection = pool.getConnection();
 			String sqlUpdate = "update company set " + "email = '" + company.getEmail() + "', " + "password = '"
 					+ company.getPassword() + "' " + "where id =" + company.getId();
-			Statement statement = conn.createStatement();
+			Statement statement = connection.createStatement();
 
 			if (statement.executeUpdate(sqlUpdate) == 0) {
 				throw new ManagerSQLException("No records were updated");
@@ -142,7 +165,7 @@ public class CompanyDBDAO implements CompanyDAO {
 		} catch (SQLException e) {
 			throw new ManagerSQLException(e.getMessage());
 		} finally {
-			pool.returnConnection(conn);
+			pool.returnConnection(connection);
 		}
 
 	}
@@ -159,10 +182,10 @@ public class CompanyDBDAO implements CompanyDAO {
 	@Override
 	public Company getCompany(long id) throws ManagerSQLException, ManagerThreadException {
 		Company company = null;
-		Connection conn = null;
+		Connection connection = null;
 		try {
-			conn = pool.getConnection();
-			Statement statement = conn.createStatement();
+			connection = pool.getConnection();
+			Statement statement = connection.createStatement();
 
 			ResultSet resultSet = statement.executeQuery("select * from company where id=" + id);
 			if (resultSet.next()) {
@@ -179,7 +202,7 @@ public class CompanyDBDAO implements CompanyDAO {
 		} catch (SQLException e) {
 			throw new ManagerSQLException(e.getMessage());
 		} finally {
-			pool.returnConnection(conn);
+			pool.returnConnection(connection);
 		}
 
 		return company;
@@ -267,15 +290,17 @@ public class CompanyDBDAO implements CompanyDAO {
 	public Collection<Coupon> getCoupons(Company company) throws ManagerSQLException, ManagerThreadException {
 		Collection<Coupon> coupons = null;
 		Connection conn = null;
+		String sql = "select * from company_coupon cc"
+				+ " join coupon c on cc.coupon_id = c.id"
+				+ " where comp_id="+ company.getId();
 		try {
 			coupons = new ArrayList<Coupon>();
 			conn = pool.getConnection();
 			CouponDBDAO couponDB = new CouponDBDAO();
 			Statement statement = conn.createStatement();
-			ResultSet resultSet = statement.executeQuery("select * from company_coupon  where comp_id=" + company.getId());
+			ResultSet resultSet = statement.executeQuery(sql);
 			while (resultSet.next()) {
-				long couponId = resultSet.getLong("coupon_id");
-				coupons.add(couponDB.getCoupon(couponId));
+				coupons.add(getCouponFromResultSet(resultSet));
 			}
 			
 		} catch (SQLException e) {
@@ -343,6 +368,28 @@ public class CompanyDBDAO implements CompanyDAO {
 			pool.returnConnection(conn);
 		}
 
+	}
+	
+	private Company getCompanyFromResultSet(ResultSet resultSet) throws SQLException, ManagerSQLException, ManagerThreadException{
+		Company company = new Company(resultSet.getInt("id"));
+		company.setCompanyName(resultSet.getString("comp_name"));
+		company.setPassword(resultSet.getString("password"));
+		company.setEmail(resultSet.getString("email"));
+		company.setCoupons(getCoupons(company));
+		return company;
+	}
+	
+	private Coupon getCouponFromResultSet(ResultSet resultSet) throws SQLException{
+		Coupon coupon = new Coupon(resultSet.getInt("id"));
+		coupon.setAmount(resultSet.getInt("amount"));
+		coupon.setEndDate(resultSet.getDate("end_date"));
+		coupon.setStartDate(resultSet.getDate("start_date"));
+		coupon.setImage(resultSet.getString("image"));
+		coupon.setMessage(resultSet.getString("message"));
+		coupon.setPrice(resultSet.getDouble("price"));
+		coupon.setTitle(resultSet.getString("title"));
+		coupon.setType(CouponType.valueOf(resultSet.getString("type")));
+		return coupon;
 	}
 
 }
